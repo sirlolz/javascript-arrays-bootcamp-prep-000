0f8bd6640acb457172ed830026b88ae7b838fa16
Jared Burnham <zlolris97@gmail.com>
2019-06-20 17:06:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,8 +15,8 @@ function destructivelyAddElementToBeginningOfArray(array,element){
   return array
 }
 function addElementToEndOfArray(array,element){
-  array.push(element)
-  return array
+  
+  return [...array, element]
 }
 function destructivelyAddElementToEndOfArray(array,element){
   array.push(element)

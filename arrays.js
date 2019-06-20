3bb5ca99241f7f0a201820a276ad766e57776478
Jared Burnham <zlolris97@gmail.com>
2019-06-20 16:49:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,6 +11,7 @@ var aray=array.unshift(element)
 
 function destructivelyAddElementToBeginningOfArray(array,element){
   array.unshift(element)
+  return array
 }
 function addElementToEndOfArray(array,element){
   
@@ -20,4 +21,7 @@ function destructivelyAddElementToEndOfArray(array,element){
 }
 function accessElementInArray(array,index){
   return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  
 }

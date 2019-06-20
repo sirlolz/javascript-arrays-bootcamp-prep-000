2370ca46f930e4dca8c0bb6832be59c05287d076
Jared Burnham <zlolris97@gmail.com>
2019-06-20 17:04:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,6 +32,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
-//function destructivelyRemoveElementFromEndOfArray(array){
-  //
-//}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop(-1)
+}

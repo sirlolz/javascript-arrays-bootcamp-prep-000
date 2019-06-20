0782f6c46c001bb71522873f3548382fd5db0ea8
Jared Burnham <zlolris97@gmail.com>
2019-06-20 16:44:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,6 +9,6 @@ function addElementToBeginningOfArray(array, element){
 var aray=array.unshift(element)
 }
 
-//function destructivelyAddElementToBeginningOfArray(array,element){
-//  array.unshift(element)
-//}
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element)
+}
